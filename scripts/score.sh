08f754df6ec4bdc14bbd6cf7b981b110611f1cf7
HexagonStar <shawnxzhu@gmail.com>
2025-04-14 16:33:39 +0800

    update script

--- scripts/score.sh
+++ scripts/score.sh
@@ -9,7 +9,7 @@ GPT-4o-mini-2024-07-18
 
 for model_name in ${MODEL_LIST[@]}
 do
-    python ./src/score.py --dataset_name ./datasets/preference20K/${model_name} \
+    python ./src/score.py --dataset_name ./datasets/preference20K/CHARM-${model_name} \
                               --model_name ${RM_NAME} \
                               --output_path ./datasets/scores/${RM_NAME}/${model_name}_score \
                               --config_path ./configs/rm_config.json
